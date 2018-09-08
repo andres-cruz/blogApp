7bfff218a55f9290538d136c33a7c42baf7c0ae4
Andres <andrescruz.tech@gmail.com>
2018-09-08 11:04:32 -0300

    Finished.

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ var express = require('express'),
 app = express(),
 bodyParser = require('body-parser'),
 methodOverride = require('method-override'),
-mongoose = require('mongoose');
+mongoose = require('mongoose'),
+expressSanitizer = require('express-sanitizer');
 
 // APP CONFIG
 mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true });
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride('_method'));
 
 // MONGOOSE MODEL CONFIG
@@ -45,6 +47,7 @@ app.get('/blogs/new', function(req, res){
 
 // CREATE ROUTES
 app.post('/blogs', function(req, res){
+  req.body.blog.body = req.sanitize(req.body.blog.body)
   Blog.create(req.body.blog, function(err, newBlog){
     if(err){
       res.render('new');
@@ -78,6 +81,7 @@ app.get('/blogs/:id/edit', function(req, res){
 
 // UPDATE ROUTE
 app.put('/blogs/:id', function(req, res){
+  req.body.blog.body = req.sanitize(req.body.blog.body)
   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
     if (err) {
       res.redirect('/blogs');
@@ -85,6 +89,17 @@ app.put('/blogs/:id', function(req, res){
       res.redirect('/blogs/' + req.params.id);
     }
   });
+});
+
+// DELETE ROUTE
+app.delete('/blogs/:id', function(req, res){
+  Blog.findByIdAndRemove(req.params.id, function(err){
+    if(err){
+      res.redirect('/blogs');
+    } else {
+      res.redirect('/blogs');
+    }
+  })
 });
 
 app.listen(3000, function(){
